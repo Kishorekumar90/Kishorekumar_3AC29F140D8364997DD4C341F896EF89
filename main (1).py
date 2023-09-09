59781856c164b17b3ba09e41206dd6fef73124bb
Kishorekumar90 <144331227+Kishorekumar90@users.noreply.github.com>
2023-09-09 09:41:18 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,16 @@
+#write a program that determines  weather a year entered by the user is a leap year or not using if elif-else statements. 
+
+def isLeapYear(year):
+  if(year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+   return True
+  else:
+   return False
+
+year = int(input(" Enter a year: "))
+
+if isLeapYear(year):
+  print(' {} is a leap year.'.    format(year))
+else:
+  print('{} is not a  leapyear. '.format(year))
+  
+   
